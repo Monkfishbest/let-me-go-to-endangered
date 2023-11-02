788f29d7ca8dfb5b2acee5c7aa3d89842012dafe
John <shetlandjohnmcneill@hotmail.com>
2023-11-02 13:51:41 +0000

    tidyed code, fixed bug, now runs in headless mode by deafult, manually checking this works now

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # let-me-go-to-class
-I want to go to class, but I don't want to check every 5 mins, this script will log you in, and then periodically refresh the class page and try and sign up. 
-The program wil terminate when you have signed up. 
+I want to go to class, but I don't want to check every 5 mins, this script will log you in, and then periodically refresh the class page and try and sign in to the particular class. 
+
+It runs a chrome browser in headless mode, so it sits quietly in the background. 
+
+The program will terminate when you have signed up. 
 
 ## required tech for mac 
+
 Python3 
 PiP3 (should come with python 3) 
 brew 
@@ -11,15 +15,12 @@ chrome
 
 ## start guide 
 
-go to line 21, add the class you want to sign up to but some bastard(s) have already made it full, surely one of them will quit at the last minute, but you will be fucked if you want to check in every 5 mins, so let python do it for you. 
-
 to install dependencies 
-if you're on Linux/windows/wsl figure that stuff out yourself ;) 
+
 `pip3 install selenium` 
 `brew install webdriver` 
 
 ## Run code 
-If you want to run it in headless, i've added the code that I think will do in a comment, I have not checked that so it might not work but I don't see why I wouldn't, maybe the site will not let you use it in headless mode, I dunno, I presume you will know enough to fix it if you know what headless mode is. otherwise... 
 
 `python3 letMeGoToClass.py`
 and watch the magic happen. 
--- letMeGoToClass.py
+++ letMeGoToClass.py
@@ -6,19 +6,16 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
 
-# get rid of this code if you don't need to hide your details
 hello_club_password = os.getenv("HELLO_CLUB_PASS")
 hello_club_email = os.getenv("HELLO_CLUB_EMAIL")
 
-# If you want to run it in headless mode, It might work, I cba to check ¯\_(ツ)_/¯ fight me. 
-# chrome_options = Options()
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--disable-gpu")
-# driver = webdriver.Chrome( options=chrome_options)
 
-driver = webdriver.Chrome()
-# add the event you want to go here in "quotation marks "
-driver.get("https://app.helloclub.com/events/view/61b9c7983adbd9c92dd3673a")
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+driver = webdriver.Chrome( options=chrome_options)
+
+driver.get("https://app.helloclub.com/events/view/61b9c7983adbd9c92dd3673c")
 
 
 
@@ -33,43 +30,42 @@ username_login_form_input =  WebDriverWait(driver,10).until(
 password_login_form_input =  driver.find_element(By.CSS_SELECTOR, "body > ui-view > portal-route > div > div > login-credentials-route > div > section > form > div:nth-child(2) > input")
 
 
-# Put login details here 
-username_login_form_input.send_keys(hello_club_email) # email goes in a "string" 
-password_login_form_input.send_keys(hello_club_password) # password goes in a "string"
+username_login_form_input.send_keys(hello_club_email) 
+password_login_form_input.send_keys(hello_club_password) 
 
 
 login_button = driver.find_element(By.CSS_SELECTOR, "body > ui-view > portal-route > div > div > login-credentials-route > div > section > form > div:nth-child(3) > button")
 login_button.click()
-time.sleep(5)
+time.sleep(3)
 
 has_signed_in = True
 while has_signed_in:
     try:
         sign_up_button = driver.find_element(By.CSS_SELECTOR, "body > ui-view > app-route > div > main > ui-view > event-view-route > div > div > cards > div > card:nth-child(2) > div > card-event-summary > section > div.EventOptions.ng-scope > div:nth-child(1) > button")
-        print("first thing works")
         
-        print(sign_up_button.text)
 
         if(sign_up_button.text == "event_available SIGN UP"):
             
             sign_up_button.click()
             print("Slot available! Booking now...")
             
-            time.sleep(5)
+            time.sleep(3)
 
             sign_up_button_confirm = driver.find_element(By.CSS_SELECTOR, "body > ui-view > app-route > div > main > ui-view > event-signup-route > div > div > cards > div > card:nth-child(2) > div > card-event-signup-details > card-footer > footer > button-bar > div > div > button.Button.Button--success.ng-animate-disabled")
             sign_up_button_confirm.click()
-            time.sleep(5)
+            time.sleep(3)
 
             has_signed_in = False
             print("Closing letMeGoTOClass...")
         else:
             print("No slots available. Retrying in 5 minutes...")
-            time.sleep(300)
+            time.sleep(295)
             driver.refresh()
+            time.sleep(5)
 
 
     except Exception as somethingWrong:
         print(somethingWrong)
-        print("it broke, oh dear :( ")
+        print("it broke, oh dear :( ")            
+
 
